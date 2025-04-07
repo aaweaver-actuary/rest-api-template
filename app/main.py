42d376dc1d1751dc295dc2f77d322337730e2138
Andy Weaver <andrewayersweaver@gmail.com>
2025-04-07 14:44:21 +0000

    moving back to laptop

--- app/main.py
+++ app/main.py
@@ -2,3 +2,10 @@ import streamlit as st
 import requests
 
 st.title("Simple Pricing Model API")
+
+# Side-by-side input boxes for base rate and mod factor:
+col1, col2 = st.columns(2)
+with col1:
+    base_rate = st.number_input("Base Rate", min_value=0.0, format="%.2f")
+with col2:
+    mod_factor = st.number_input("Mod Factor", min_value=0.0, format="%.2f")
